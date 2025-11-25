c9731adde3fa18293f82747497f4cf2934bbea9f
Ivelina Mircheva <stu2301321040@uni-plovdiv.bg>
2025-11-25 18:18:53 +0200

    Added new activity and relate it to the main.

--- app/src/main/java/com/example/mobileapps2025_2301321040/MainActivity.java
+++ app/src/main/java/com/example/mobileapps2025_2301321040/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.mobileapps2025_2301321040;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -8,13 +9,24 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton fabAdd;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        fabAdd = findViewById(R.id.fabAddBook);
+        //Relate activities
+        fabAdd.setOnClickListener(v -> {
+            startActivity(new Intent(MainActivity.this, BookFormActivity.class));
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
